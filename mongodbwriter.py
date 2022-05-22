e3be72c70ebfdd32475aebd55f54a76f4fd024ca
parhambarazesh <parham.barazesh@gmail.com>
2022-05-22 15:09:56 +0200

    update mongodb writer to write into a single collection

--- mongodbwriter.py
+++ mongodbwriter.py
@@ -4,5 +4,6 @@ import pymongo
 def write_to_database(data,rule):
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["twitter_stream"]
-    collection = db["tweets_"+rule]
+    collection=db["tweets"]
+    # collection = db["tweets_"+rule]
     collection.insert_one(data)
