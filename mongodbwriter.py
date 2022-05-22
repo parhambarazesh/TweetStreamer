6b7646a2589f69157337c25a714573bc04b0d7f1
parhambarazesh <parham.barazesh@gmail.com>
2022-05-23 00:52:50 +0200

    change the directory structure

--- mongodbwriter.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-def write_to_database(data,rule):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["twitter_stream"]
-    collection=db["tweets"]
-    collection.insert_one(data)
