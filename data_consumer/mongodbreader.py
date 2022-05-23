41e2fd0ee24b0a2078fc77ddb88f1046b4bc9fae
parhambarazesh <parham.barazesh@gmail.com>
2022-05-23 22:57:45 +0200

    get the pending tweet from mongodb

--- data_consumer/mongodbreader.py
+++ data_consumer/mongodbreader.py
@@ -5,8 +5,7 @@ def read_from_database(rule):
         client = pymongo.MongoClient("mongodb://localhost:27017/")
         db = client["twitter_stream"]
         collection=db["tweets"]
-        data=collection.find_one({"rule":rule})
-        # data=collection.find_one({"rule":rule,"status":"pending"})
+        data=collection.find_one({"rule":rule,"status":"pending"})
         print("DATA:",rule,data)
         if data!=None:
             collection.update_one({"_id":data["_id"]},{"$set":{"status":"published"}})
